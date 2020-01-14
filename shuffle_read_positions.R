2c488e6811034f9ed46e032a7389a21a3660e135
Jesko Wagner <35219306+jeskowagner@users.noreply.github.com>
2020-01-14 16:14:06 +0100

    New function shuffle_sv_positions
    can also take translocation

--- shuffle_read_positions.R
+++ shuffle_read_positions.R
@@ -1,6 +1,124 @@
 ### Author : Jesko Wagner
 ### Date: 2020.01.13
 
+### Aim: shuffle positions of any genomic range across the chromosome
+### to estimate if the read is positioned at its location by chance
+### Note: currently this does not support strandedness.
+
+shuffle_sv_positions = 
+  function(sv.df,
+           genome = "hg19",
+           key.cols = key(sv.df),
+           chromosomes = paste0("chr", c(1:22, "X", "Y")),
+           chrom.sizes = NULL,
+           chrom.sizes.name.col = "UCSC_seqlevel",
+           chrom.sizes.length.col = "UCSC_seqlength",
+           verbose = F) {
+    
+    # Dependencies
+    library(GenomicRanges) # for getting chromosome sizes
+    library(data.table)
+    
+    ### Sanity checks
+    if(verbose) message("Checking data integrity.")
+    
+    if(!any(is.data.frame(sv.df) | "GRanges" %in% class(sv.df))) {
+      stop("Data must be data.frame or GRanges.")
+    }
+    
+    if(sv.df[,any(is.na(get(key.cols)))]) {
+      stop("Data must not have NA values in chr, start, or end columns.")
+    }
+    
+    # Have chromosome sizes been provided? If no, download. If yes, check data
+    if(is.null(chrom.sizes)) {
+      # Prepare, download chromosome sizes and obtain only those of required chromosomes
+      if(verbose) message("Downloading chromosome information from UCSC.")
+      
+      chrom.sizes = fetchExtendedChromInfoFromUCSC(genome, quiet = T)
+      chrom.sizes = as.data.table(chrom.sizes)
+      chrom.sizes = chrom.sizes[UCSC_seqlevel %in% chromosomes,
+                                .(UCSC_seqlevel, UCSC_seqlength)]
+      
+    } else {
+      chrom.sizes = as.data.table(chrom.sizes)  
+      
+      if(!is.null(chrom.sizes.name.col)) 
+        setnames(chrom.sizes, old = chrom.sizes.name.col, new = "UCSC_seqlevel")
+      
+      if(!is.null(chrom.sizes.length.col)) 
+        setnames(chrom.sizes, old = chrom.sizes.length.col, new = "UCSC_seqlength")
+    }
+    
+    # Check that all entered chromosome information is correct
+    if (chrom.sizes[, 
+                    !is.numeric(UCSC_seqlength) | 
+                    !(length(unique(UCSC_seqlevel)) == length(UCSC_seqlevel))
+                    ]) {
+      stop("chrom.sizes contains invalid or duplicate values.")
+    }
+    
+    
+    ### End sanity checks
+    
+    # Convert to dt for memory efficiency and joining
+    sv.df = as.data.table(sv.df)
+    
+    # Keying allows keyed join for additional speed
+    setkey(chrom.sizes, UCSC_seqlevel)
+    setkeyv(sv.df, key.cols)
+
+    # Add temporary column containing the length of the read
+    if(verbose) message("Calculating read length.")
+    
+    ### For SVs other than TRA
+    ### Note that SVs do not have an SV length, pos_alt is the new position
+    # Get indices
+    no_tra = sv.df[get(key.cols[1]) == get(key.cols[3]), which = TRUE]
+    
+    # calculate SV length
+    sv.df[no_tra, processing_width_tmp := (get(key.cols[4]) - get(key.cols[2])), by = names(sv.df)]
+    
+    
+    # Left join the chromosome sizes for chrom_ref
+    sv.df[chrom.sizes, processing_width_chr1 := UCSC_seqlength]
+    
+    # Left join the chromosome sizes for chrom_alt
+    setkeyv(sv.df, c(key.cols[3], key.cols[4], key.cols[1], key.cols[2]))
+    sv.df[chrom.sizes, processing_width_chr2 := UCSC_seqlength]
+    
+    setkeyv(sv.df, key.cols) # return to original sorting
+    
+    # Calculate new start positions
+    if(verbose) message("Assigning new read positions.")
+    
+    # For svs other than TRA
+    sv.df[no_tra,
+             # Generate random number between 1 and the size of the chromosome minus the width of the read
+             eval(key.cols[2]) := round(digits = 0,
+                                      runif(n = 1,
+                                            min = 1,
+                                            max = processing_width_chr1 - processing_width_tmp - 1 # honor 1-based coordinates
+                                      )), 
+             by = names(sv.df)
+             ]
+    
+    # Add the new end postion of the read based on the previously calculated width
+    sv.df[no_tra, eval(key.cols[4]) := get(key.cols[2]) + processing_width_tmp, ]
+    
+    # For TRA, just assign random positions
+    sv.df[!no_tra, eval(key.cols[2]) := round(digits = 0, runif(n = 1, min = 1, max = processing_width_chr1)), names(sv.df)]
+    sv.df[!no_tra, eval(key.cols[4]) := round(digits = 0, runif(n = 1, min = 1, max = processing_width_chr2)), names(sv.df)]
+    
+    # Remove temporary columns
+    sv.df[, processing_width_tmp := NULL]
+    sv.df[, processing_width_chr1 := NULL]
+    sv.df[, processing_width_chr2 := NULL]
+    
+    # Return
+    sv.df[]
+  }
+
 
 
 ### Aim: shuffle positions of any genomic range across the chromosome
